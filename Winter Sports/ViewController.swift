d44d8c09fba3c7473efdca5650d286d54083070f
ART_List <artbdarm9@mail.ru>
2020-12-15 20:56:55 +0300

    first commit

---
+++ Winter Sports/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Winter Sports
+//
+//  Created by Artem Listopadov on 12/15/20.
+//  Copyright © 2020 Artem Listopadov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
